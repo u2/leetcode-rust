b2e4472d5c810b120ea357e7f1db00c56eba1f88
u2 <zhangyaning1985@gmail.com>
2017-01-15 20:48:04 +0800

    add two number optimizing

--- src/add_two_numbers.rs
+++ src/add_two_numbers.rs
@@ -3,19 +3,26 @@ pub fn add_two_numbers(l1: &[usize], l2: &[usize]) -> Vec<usize> {
         true => l1.len(),
         false => l2.len(),
     };
-    let mut vec: Vec<usize> = Vec::with_capacity(length+1);
+    let mut vec: Vec<usize> = Vec::with_capacity(length + 1);
     let mut a: usize;
     let mut b: usize;
     let mut c: usize;
-    let mut x: usize = 0;
-    for i in 0..length+1 {
-        a = if i < l1.len() { l1[i] } else { 0 };
-        b = if i < l2.len() { l2[i] } else { 0 };
-        c = a + b + x;
-        if c > 9 { x = 1; c = c - 10 } else { x = 0 };
-        if !(c == 0 && i == length) { vec.push(c); };
+    let mut carry: usize = 0;
+    for i in 0..length {
+        a = *l1.get(i).unwrap_or(&0);
+        b = *l2.get(i).unwrap_or(&0);
+        c = a + b + carry;
+        if c > 9 {
+            carry = 1;
+            c = c - 10;
+        } else {
+            carry = 0;
+        }
+        vec.push(c);
     }
-    println!("vec: {:?}", vec);
+    if carry == 1 {
+        vec.push(carry);
+    }
     return vec;
 }
 
@@ -25,14 +32,14 @@ mod test {
 
     #[test]
     fn test_add_two_numbers() {
-        assert_eq!(add_two_numbers(&[2,4,3], &[5,6,4]), vec![7,0,8]);
+        assert_eq!(add_two_numbers(&[2, 4, 3], &[5, 6, 4]), vec![7, 0, 8]);
 
-        assert_eq!(add_two_numbers(&[2,2], &[5,6,4]), vec![7,8,4]);
+        assert_eq!(add_two_numbers(&[2, 2], &[5, 6, 4]), vec![7, 8, 4]);
 
-        assert_eq!(add_two_numbers(&[2,2,1], &[5,6,4]), vec![7,8,5]);
+        assert_eq!(add_two_numbers(&[2, 2, 1], &[5, 6, 4]), vec![7, 8, 5]);
 
-        assert_eq!(add_two_numbers(&[2,2,1,3], &[5,6,4]), vec![7,8,5,3]);
+        assert_eq!(add_two_numbers(&[2, 2, 1, 3], &[5, 6, 4]), vec![7, 8, 5, 3]);
 
-        assert_eq!(add_two_numbers(&[9], &[9]), vec![8,1]);
+        assert_eq!(add_two_numbers(&[9], &[9]), vec![8, 1]);
     }
 }
